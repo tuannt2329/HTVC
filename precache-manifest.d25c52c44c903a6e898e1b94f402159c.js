19522d392cd72c9b2bf3cf5c2c58af8287cf23b0
tuannt2329 <nguyenanhtuan29091997@gmail.com>
2020-06-09 22:24:44 +0700

    Updates

---
+++ precache-manifest.d25c52c44c903a6e898e1b94f402159c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c23c7716dcc60b125bc79089189e1c6",
+    "url": "/HTVC/index.html"
+  },
+  {
+    "revision": "c1d2ca6ec2270b3c422a",
+    "url": "/HTVC/static/css/main.f6a7df9e.chunk.css"
+  },
+  {
+    "revision": "a26161474f656b502e6f",
+    "url": "/HTVC/static/js/2.18e9ccae.chunk.js"
+  },
+  {
+    "revision": "c1d2ca6ec2270b3c422a",
+    "url": "/HTVC/static/js/main.17f4253a.chunk.js"
+  },
+  {
+    "revision": "52575d244d7303b8fddd",
+    "url": "/HTVC/static/js/runtime-main.a41bc9ef.js"
+  }
+]);
